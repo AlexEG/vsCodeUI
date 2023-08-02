6ec8b80b50c518e87a2e71abd8c646900be01d97
Alexander EG#9072 <116421926+AlexEG@users.noreply.github.com>
2023-08-03 02:38:17 +0300

    v1.0.1

--- src/components/SidebarNavigation/NavbarIcon.tsx
+++ src/components/SidebarNavigation/NavbarIcon.tsx
@@ -1,24 +1,34 @@
-function NavbarIcon() {
+type NavbarIcon = {
+  isOpen: boolean;
+  svg: JSX.Element;
+};
+function NavbarIcon({ isOpen, svg }: NavbarIcon) {
   const lineCSS = `before:absolute before:left-0 before:top-0 before:bottom-0 before:w-[2px] before:bg-[#f78166]`;
+
+  const isOpenCSS = isOpen ? lineCSS : "";
   return (
     <button
-      className={`w-12 h-12 flex justify-center items-center relative  ${lineCSS}`}
+      className={`w-12 h-12 flex justify-center items-center relative  ${isOpenCSS}`}
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.3}
-        stroke="#7d8590"
-        className="w-7 h-7"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75"
-        />
-      </svg>
+      {svg}
     </button>
   );
 }
 export default NavbarIcon;
+<svg
+  width="64px"
+  height="64px"
+  viewBox="0 0 24 24"
+  xmlns="http://www.w3.org/2000/svg"
+  fill="#ffffff"
+>
+  <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+  <g
+    id="SVGRepo_tracerCarrier"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  ></g>
+  <g id="SVGRepo_iconCarrier">
+    <path d="M10.94 13.5l-1.32 1.32a3.73 3.73 0 0 0-7.24 0L1.06 13.5 0 14.56l1.72 1.72-.22.22V18H0v1.5h1.5v.08c.077.489.214.966.41 1.42L0 22.94 1.06 24l1.65-1.65A4.308 4.308 0 0 0 6 24a4.31 4.31 0 0 0 3.29-1.65L10.94 24 12 22.94 10.09 21c.198-.464.336-.951.41-1.45v-.1H12V18h-1.5v-1.5l-.22-.22L12 14.56l-1.06-1.06zM6 13.5a2.25 2.25 0 0 1 2.25 2.25h-4.5A2.25 2.25 0 0 1 6 13.5zm3 6a3.33 3.33 0 0 1-3 3 3.33 3.33 0 0 1-3-3v-2.25h6v2.25zm14.76-9.9v1.26L13.5 17.37V15.6l8.5-5.37L9 2v9.46a5.07 5.07 0 0 0-1.5-.72V.63L8.64 0l15.12 9.6z"></path>
+  </g>
+</svg>;
--- src/components/SidebarNavigation/SidebarNavigation.tsx
+++ src/components/SidebarNavigation/SidebarNavigation.tsx
@@ -1,9 +1,53 @@
 import NavbarIcon from "./NavbarIcon";
+import IconsSVGPaths from "./IconsSVGPaths";
+import { useState } from "react";
 function SidebarNavigation() {
+  const [sidebarNavigationState, setSidebarNavigationState] = useState({
+    openTap: 8,
+  });
+
+  function isOpenFunc(sectionNum: number) {
+    if (sidebarNavigationState.openTap === sectionNum) return true;
+    return false;
+  }
+  function changeIsOpenFunc(sectionNum: number) {
+    setSidebarNavigationState((sidebarNavigationState) => {
+      return {
+        ...sidebarNavigationState,
+        openTap: sectionNum,
+      };
+    });
+  }
   return (
-    <nav className="bg-[#0d1117] col-start-1 col-end-2 row-start-2 row-end-3 flex flex-col">
-      <NavbarIcon />
-      <NavbarIcon />
+    <nav className="bg-[#0d1117] col-start-1 col-end-2 row-start-2 row-end-3 flex flex-col justify-between">
+      <div>
+        <div onClick={() => changeIsOpenFunc(1)}>
+          <NavbarIcon isOpen={isOpenFunc(1)} svg={IconsSVGPaths.filesSVG} />
+        </div>
+        <div onClick={() => changeIsOpenFunc(2)}>
+          <NavbarIcon isOpen={isOpenFunc(2)} svg={IconsSVGPaths.testSVG} />
+        </div>
+        <div onClick={() => changeIsOpenFunc(3)}>
+          <NavbarIcon isOpen={isOpenFunc(3)} svg={IconsSVGPaths.searchSVG} />
+        </div>
+        <div onClick={() => changeIsOpenFunc(4)}>
+          <NavbarIcon isOpen={isOpenFunc(4)} svg={IconsSVGPaths.extensionSVG} />
+        </div>
+        <div onClick={() => changeIsOpenFunc(5)}>
+          <NavbarIcon isOpen={isOpenFunc(5)} svg={IconsSVGPaths.debugSVG} />
+        </div>
+        <div onClick={() => changeIsOpenFunc(6)}>
+          <NavbarIcon isOpen={isOpenFunc(6)} svg={IconsSVGPaths.gitSVG} />
+        </div>
+      </div>
+      <div>
+        <div onClick={() => changeIsOpenFunc(7)}>
+          <NavbarIcon isOpen={isOpenFunc(7)} svg={IconsSVGPaths.accountSVG} />
+        </div>
+        <div onClick={() => changeIsOpenFunc(8)}>
+          <NavbarIcon isOpen={isOpenFunc(8)} svg={IconsSVGPaths.settingsSVG} />
+        </div>
+      </div>
     </nav>
   );
 }
